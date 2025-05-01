c3fdd462fa58fe1a5bc128dc535cea7ef52d134a
Margo099 <gooyeontime.gg@gmail.com>
2025-05-01 11:47:26 +0300

    new features added

--- LibraryStorage/LibraryStorage.sol
+++ LibraryStorage/LibraryStorage.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
-
 contract LibraryStorage {
+    mapping(uint => address[]) reservations;
     mapping(address => uint) public bookOwner;
     struct Book{
         string bookName;
@@ -16,7 +16,7 @@ contract LibraryStorage {
             bookName: _bookName,
             owner: address(0)
         }));
-        
+    
         emit newBookAdded(_bookName);
     }
     function borrowBook(uint bookId) external {
@@ -39,7 +39,6 @@ contract LibraryStorage {
                 count++;
             }
         }
-
         Book[] memory availableBooks = new Book[](count);
         uint index = 0;
         for (uint i = 0; i < books.length; i++) {
@@ -48,7 +47,6 @@ contract LibraryStorage {
                 index++;
             }
         }
-
         return availableBooks;
     }
     function allReservedBooks() external view returns(Book[] memory) {
@@ -58,7 +56,6 @@ contract LibraryStorage {
             count++;
         }
     }
-    
     Book[] memory reservedBooks = new Book[] (count);
     uint index = 0;
     for(uint i = 0; i < books.length; i++){
@@ -67,4 +64,29 @@ contract LibraryStorage {
     }
     return reservedBooks;
     }
+    function reserv(uint bookId) external {
+        require(books[bookId].owner != address(0), "Book is free to be taken");
+        reservations[bookId].push(msg.sender);
+    }
+    function getMyReservations(uint bookId) external view returns(address[] memory) {
+    return reservations[bookId];
+    } 
+    function cancelReservation(uint bookId) external {
+        address[] storage list = reservations[bookId];
+        uint length = list.length;
+        require(length > 0, "No reservations found!");
+
+        bool found = false;
+        for(uint i=0; i < length; i++) {
+            if(list[i]== msg.sender){
+                for(uint j=i; j<length; j++){
+                    list[j]= list[j+1];
+                }
+                list.pop();
+                found=true;
+                break;
+            }
+        }
+        require(found, "Reservation not found!");
+}
 }
