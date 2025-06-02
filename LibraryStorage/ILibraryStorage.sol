f8123965b5c6902e5d2b0e62b0bd4db7a42cc465
Margarita <gooyeontime.gg@gmail.com>
2025-06-02 20:16:49 +0300

    Delete LibraryStorage directory

--- LibraryStorage/ILibraryStorage.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-interface ILibraryStorage {
-    function borrowBook(uint bookId) external;
-    function returnBook(uint bookId) external;
-    function reserveBook(uint bookId) external;
-    function cancelReservation(uint bookId) external;
-}
